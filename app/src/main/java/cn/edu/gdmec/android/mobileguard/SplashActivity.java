64bc6510b18ecb3d19dbb1b8a94f4d2a58387d71
Rysj0 <1161297560@qq.com>
2017-10-11 17:40:25 +0800

    改包名

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -0,0 +1,33 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+import cn.edu.gdmec.android.mobileguard.m1home.utils.Myutils;
+import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView mTvTextView;
+    private String mVersion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mVersion = Myutils.getVersion(getApplicationContext());
+        mTvTextView = (TextView)findViewById(R.id.tv_splash_version);
+        mTvTextView.setText("版本号"+mVersion);
+        final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+        new Thread(){
+            @Override
+            public void run(){
+                super.run();
+                versionUpdateUtils.getCloudVersion();
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,9 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+public class VersionEntity {
+    public String versionCode;
+    public String description;
+    public String apkurl;
+
+
+}
