cf9e0ece370b39775d2b13e83d2218be1758d622
li0725 <1004511748@qq.com>
2017-10-14 13:24:51 +0800

    10.13  手机防盗 again5

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -15,8 +15,6 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
-
-
 import cn.edu.gdmec.android.mobileguard.m2thefthgurd.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2thefthgurd.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2thefthgurd.dialog.SetUpPasswordDialog;
