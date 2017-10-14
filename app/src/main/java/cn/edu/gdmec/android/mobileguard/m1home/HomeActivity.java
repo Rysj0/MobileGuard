cfdca9fe0be0799346a6ea112208fda2a54f686e
li0725 <1004511748@qq.com>
2017-10-14 14:06:39 +0800

    10.13  手机防盗       again8

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -15,9 +15,9 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
-import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetUpPasswordDialog;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
 
 public class HomeActivity extends AppCompatActivity {
@@ -122,7 +122,7 @@ public class HomeActivity extends AppCompatActivity {
                 }else if (password.equals(MD5Utils.encode(mInPswdDialog.getPassword()))){
                     //进入防盗主界面
                     mInPswdDialog.dismiss();
-                   startActivity(LostFindActivity.class);
+                 startActivity(LostFindActivity.class);
                     Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
                 }else{
                     //对话框消失，弹出土司
